4eab3f7dc465f0fa2d80a63677b6c7eaddbd5c46
Sarah <74516787+Sbethm@users.noreply.github.com>
2021-10-11 16:32:20 -0500

    encription
    btoa encryption was added to encode the messages into a link later

--- .history/apps_20211011162928.js
+++ .history/apps_20211011162928.js
@@ -2,7 +2,7 @@ document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault();
 
     const input = document.querySelector('.message-input');
-    const encrypted = btoa(input.value);
+    const encrypted = bto;
 
 
     document.querySelector('#link-input').value = encrypted;
